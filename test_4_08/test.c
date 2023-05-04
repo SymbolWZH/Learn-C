6fd5cd73556592899008971b2be656da58dac970
SymbolWZH <zh1710396939@163.com>
2023-05-04 10:38:05 +0800

    修改

--- test_4_08/test.c
+++ test_4_08/test.c
@@ -19,11 +19,11 @@
 //     return 0;
 // }
 // extern int g_val;
-int main()
-{
-    printf("%d\n",g_val);
-    return 0;
-}
+// int main()
+// {
+//     printf("%d\n",g_val);
+//     return 0;
+// }
 // ##### 数据类型
 
 // ```
@@ -52,3 +52,218 @@ int main()
 // 枚举常量 可以一一列举的常量 默认从0开始可以赋初始值 enum Gender{MALE}
 // 字符串默认隐藏/0 
 // ```
+//写一个函数求两个整数的较大值
+//如：输入：10 20输出较大值：20
+// int main()
+// {
+//     int i=0;
+//     int j=0;
+//     scanf("%d %d",&i,&j);
+//     if(i>j)
+//         return i;
+//     else
+//         return j;
+// }
+//给定两个数，求这两个数的最大公约数
+// #include <stdio.h>
+// int gcd(int a, int b ) {
+//    if(b%a==0){
+//         return a;
+//     }
+//     else{ 
+//         return gcd(b%a,a);
+//     }
+// }
+// //打印1000年到2000年之间的闰年
+// int main()
+// {
+//     unsigned int i =0;
+//     for ( i = 1000; i <=2000; i++)
+//     {
+//        if(i % 4 == 0 )
+//        {
+//         if(i%100!=0) // 如果year不能内100整除，则一定是闰年
+// 			{
+// 				printf("%d ", i);
+// 			}
+//        }
+//        if(i%400 == 0)  // 每400年再润一次
+// 		{
+// 			printf("%d ", i);
+// 		}
+//     }
+//     return 0;
+// }
+//写一个代码：打印100~200之间的素数
+// int main()
+// {
+//  	int i = 0;//
+// 	int count = 0;
+
+
+// 	for(i=101; i<=200; i++)
+// 	{
+// 		//判断i是否为素数
+// 		//2->i-1
+// 		int j = 0;
+// 		for(j=2; j<=i/2; j++)
+// 		{
+// 			if(i%j == 0)
+// 			{
+// 				break;
+// 			}
+// 		}
+// 		//...
+// 		if(j>i/2)
+// 		{
+// 			count++;
+// 			printf("%d ", i);
+// 		}
+// 	}
+// 	printf("\ncount = %d\n", count);
+// 	return 0;
+// }
+//写代码将三个整数数按从大到小输出。
+// void print(int a,int b,int c)
+// {
+//     if(a<b)
+//     {
+//         int tmp = a;
+//         a=b;
+//         b=tmp;
+//     }
+//     if(a<c)
+//     {
+//         int tmp = a;
+//         a=c;
+//         c=tmp;
+//     }
+//     if(b<c)
+//     {
+//         int tmp = b;
+//         b=c;
+//         c=tmp;
+//     }
+//      printf("a=%d b=%d c=%d\n", a, b, c);
+    
+// }
+// int main(){
+//     int a=0;
+//     int b=0;
+//     int c=0;
+//     scanf("%d %d %d",&a,&b,&c);
+//     print(a,b,c);
+//     return 0;
+// }
+
+//写一个代码打印1-100之间所有3的倍数的数字
+// int main()
+// {
+//     for (size_t i = 1; i < 100; i++)
+//     {
+//         if(i % 3 == 0)
+//             printf("%d ",i);
+//     }
+    
+// }
+//编写代码在一个整形有序数组中查找具体的某个数
+// int main()
+// {
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+// 	int key = 3;
+// 	int left = 0;
+// 	int right = sizeof(arr)/sizeof(arr[0]); // right位置的数据取不到
+
+// 	while(left<right) // right位置没有数据，此处不需要添加=
+// 	{
+// 		int mid = left+(right-left)/2;
+// 		if(arr[mid]>key) // key小于中间位置数据，说明key可能在左半侧，需要改变右边界
+// 		{
+// 			right = mid; // right位置的数据取不到，因此right=mid，不需要减1
+// 		}
+// 		else if(arr[mid]<key)// key大于中间位置数据，说明key可能在右半侧，需要改变左边界
+// 		{
+// 			left = mid+1; // left位置的数据可以取到，因此left=mid+1
+// 		}
+// 		else
+// 		{
+// 			printf("找到了，下标是：%d\n", mid);
+//             break;
+// 		}
+// 	}
+   
+// 	if(left>=right)
+// 		printf("找不到\n");
+// 	return 0;
+// }
+//编写程序数一下 1到 100 的所有整数中出现多少个数字9
+// int main()
+// {
+//     int num =0;
+//     for (size_t i = 0; i < 100; i++)
+//     {
+
+//             if(i / 10  == 9  )
+//                 num++;
+//             if(i % 10 == 9  )
+//                 num++;
+        
+//     }
+//     printf("%d",num);
+//     return 0 ;
+// }
+
+//分数求和【题目内容】计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值，打印出结果
+/*
+思路：
+1. 从上述表达式可以分析出
+   a. 该表达式主要由100项，基数项为正，偶数项为负
+2. 设置一个循环从1~100，给出表达式中的每一项：1.0/i, 注意此处不能使用1，否则结果全部为0
+    然后使用flag标记控制奇偶项，奇数项为正，偶数项为负
+    然后将所有的项相加即可
+*/
+// int main()
+// {
+//     double num = 0.0;
+//     int i=0;
+//     int flag=1;
+//     for ( i = 1; i <=100; i++)
+//     {
+//         num += flag*1.0/i;
+//         flag = -flag;
+//     }
+//     printf("%lf",num);
+//     return 0;
+// }
+//求10 个整数中最大值
+// int main()
+// {
+//     int arr[10] = {0};
+//     int i = 0;
+//     for(i=0; i<10; i++)
+// 	{
+// 		scanf("%d", &arr[i]);
+// 	}
+//     int tmp = arr[0];
+//     for ( i = 1; i < 10; i++)
+//     {
+//        if(arr[i] > tmp)
+//             tmp = arr[i];
+//     }
+//     printf("%d",tmp);
+//     return 0;
+// }
+//在屏幕上输出9*9乘法口诀表
+int main()
+{   
+    int i = 0;
+    for ( i = 1; i <= 9; i++)
+    {
+        int j=0;
+        for ( j = 1; j <i+1; j++)
+        {
+            printf("%dx%d=%d ",i,j,i*j);
+        }
+        printf("\n");
+    }
+}
--- test_4_29/test.c
+++ test_4_29/test.c
@@ -118,14 +118,253 @@ int main()
 }
 */
 
+/*
+
 int main()
 {
     int arr[10] = {1,2,3,4,5,6,7,8,9};
-    int (*pa)[10] = &arr;
+    int (*pa)[10] = &arr;//(*pa)[10]相当于拿到整个数组
     int i = 0;
     for ( i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++)
     {
         printf("%d ", *(*pa+i));//1 2 3 4 5 6 7 8 9 0 
     }
     return 0;
+}
+*/
+
+/*
+void print1(int arr[3][5],int row,int col)
+{
+    int i=0,j=0;
+    for ( i = 0; i < row; i++)
+    {
+        for ( j = 0; j < col; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+void print2(int (*p)[5],int row,int col)//(*p)表示第一行首地址
+{
+    int i=0,j=0;
+    for ( i = 0; i < row; i++)
+    {
+        for ( j = 0; j < col; j++)
+        {
+            printf("%d ",*(*(p+i)+j));
+        }
+        printf("\n");
+    }
+    
+}
+int main()
+{
+    int arr[3][5] = {{1,2,3,4,5},{3,4,5,6,7},{5,6,7,8,9}};
+    print1(arr,3,5);
+    print2(arr,3,5);
+    return 0;
+}
+*/
+
+/*
+int arr[5];//整型数组
+int *parr1[10];//整型指针的数组
+int (*parr2)[10];//数组指针，该指针能够指向一个数组，数组10个元素，每个元素的类型是int
+int (*parr3[10])[5];//parr3是一个存储数组指针的数组该数组能够存放10个数组指针
+//每个数组指针能够指向一个数组，数组五个元素 ，每个元素是int类型
+*/
+
+/*
+ 一维数组传参
+void test(int arr[])//ok? ok
+{}
+void test(int arr[10])//ok? ok
+{}
+void test(int *arr)//ok? ok//首元素地址
+{}
+void test2(int *arr[20])//ok?  ok
+{}
+void test2(int **arr)//ok? ok
+{}
+int main()
+{
+ int arr[10] = {0};
+ int *arr2[20] = {0};//整型指针数组
+ test(arr);
+ test2(arr2);
+}
+*/
+
+/*
+//二维数组传参
+void test(int arr[3][5])//ok？ok
+{}
+void test(int arr[][])//ok？no
+{}
+void test(int arr[][5])//ok？ok
+{}
+//总结：二维数组传参，函数形参的设计只能省略第一个[]的数字。
+//因为对一个二维数组，可以不知道有多少行，但是必须知道一行多少元素。
+//这样才方便运算。
+void test(int *arr)//ok？ no
+{}
+void test(int* arr[5])//ok？ ok
+{}
+void test(int (*arr)[5])//ok？ ok
+{}
+void test(int **arr)//ok？ no
+{}
+int main()
+{
+ int arr[3][5] = {0};
+ test(arr);
+}
+*/
+
+//做题
+/*
+int i;//默认为0
+int main()
+{
+    i--;//-1 
+    if(i > sizeof(i))
+    //sizeof是以一个unsgint 类型  比较的时候会默认转成unsgint 类型所以会变成一个很大的数字
+    //如果是 sizeof(i) > i unsgint 类型会转成int类型
+        printf(">\n");
+    else
+        printf("<\n");
+}
+*/
+
+//给定一个数字输出他的和
+//例  2+22+222+2222+22222
+/*
+int main()
+{
+    int a =0;
+    int n = 0;
+    scanf("%d %d",&a,&n);
+    int i=0;
+    int ret = 0;
+    int sum = 0;
+    for ( i = 0; i < n; i++)
+    {
+        ret = ret * 10 +a;
+        sum +=ret;
+    }
+    printf("%d",sum);
+}
+*/
+/*
+//不使用下标打印出来数组
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int* p = &arr;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i = 0;
+    for ( i = 0; i < n; i++)
+    {
+       // printf("%d ",(*p+i));
+        printf("%d ",*(p+i));
+    }
+    
+    return 0;
+}
+*/
+/*
+#include <math.h>
+//求0-100000之间的所有”水仙花数“并输出
+//例 153 = 1 ^ 3  + 5 ^ 3 + 3 ^ 3
+int main()
+{
+    int j = 0;
+    for ( j = 0; j <= 100000; j++)
+    {
+        int n =1;//计算j的位数
+        int tmp = j;
+        while (tmp /10)
+        {   
+            n++;
+            tmp = tmp/10;
+        }
+        //计算j的每一位次方之和
+        tmp = j;
+        int sum = 0;
+        while (tmp)
+        {
+            sum += pow(tmp % 10 , n);
+            tmp = tmp / 10;
+        }
+        if(sum == j)
+            printf("%d ",j);
+    }
+    return 0;
+}
+*/
+/*
+//打印一个菱形
+void print(int m)
+{
+    int line = m;//上面的行数
+    int i = 0;
+    for ( i = 0; i < line; i++)
+    {
+        int j = 0;
+        for ( j = 0; j <line -1 -i; j++)
+        {
+            printf(" ");
+        }
+        for ( j = 0; j < 2 *i +1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    //下
+    for ( i = 0; i < line-1; i++)
+    {
+        int j = 0;
+        for ( j = 0; j <=i; j++)
+        {
+            printf(" ");
+        }
+        for ( j = 0; j < 2 *(line -1 - i) -1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+int main()
+{
+    int i = 0;
+    scanf("%d",&i);
+    print(i);
+}
+*/
+//喝汽水 1瓶1元 2个空瓶可以换1瓶 给20 可以多少喝多少瓶
+void total(int price,int num,int money)
+{
+    int sum = money / price;
+    int totals = sum;
+    while (totals >= 2)
+    {
+        sum+=totals / 2;
+        totals = totals / 2 + totals % 2;
+    }
+    printf("%d",sum);
+}  
+int main()
+{
+    int price = 0;
+    int num = 0;
+    int money = 0;
+    scanf("%d %d %d",&price,&num,&money);    
+    total(price,num,money);
+    return  0;
 }
